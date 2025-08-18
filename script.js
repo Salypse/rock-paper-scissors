e052b1ffa7f223ea57297a2cb4cfed4239eba03a
Salypse <84153483+Salypse@users.noreply.github.com>
2025-08-18 10:48:59 -0500

    Create rps game logic

--- script.js
+++ script.js
@@ -18,3 +18,50 @@ function getPlayerChoice() {
     playerInput = prompt("Choose Rock, Paper, or Scissors")
     return playerInput.toLowerCase()
 }
+
+let playerScore = 0
+let computerScore = 0
+
+//Play round of rps using player and computer choices
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) {
+        console.log("Draw")
+    }
+    else if (playerChoice == "rock" && computerChoice == "scissors") {
+        playerScore += 1
+        return console.log("You win!")
+    }
+    else if (computerChoice == "rock" && playerChoice == "scissors") {
+        computerScore += 1
+        return console.log("Computer wins!")
+    }
+    else if (playerChoice == "paper" && computerChoice == "rock") {
+        playerScore += 1
+        return console.log("You win!")
+    }
+    else if (computerChoice == "paper" && playerChoice == "rock") {
+        computerScore += 1
+        return console.log("Computer wins!")
+    }
+    else if (playerChoice == "scissors" && computerChoice == "paper") {
+        playerScore += 1
+        return console.log("You win!")
+    }
+    else if (computerChoice == "scissors" && playerChoice == "paper") {
+        computerScore += 1
+        return console.log("Computer wins!")
+    }
+
+    finishRound()
+}
+
+
+//Player and computer get new choices after each round
+function finishRound() {
+    playerChoice = getPlayerChoice()
+    computerChoice = getComputerChoice()
+}
+
+
+const playerChoice = getPlayerChoice()
+const computerChoice = getComputerChoice()
